331cda8f814b51a52bb029fdcf9a6b10a5b0fb52
shinofara <shinofara@gmail.com>
2016-06-24 23:21:06 +0900

    tarの場合でもディレクトリが格納されるように対応

--- archiver/compressor/tar.go
+++ archiver/compressor/tar.go
@@ -3,8 +3,10 @@ package compressor
 import (
 	"archive/tar"
 	"compress/gzip"
+	"fmt"
 	"io"
-	"io/ioutil"
+	"log"
+	"os"
 )
 
 type TarCompressor struct{}
@@ -17,20 +19,39 @@ func (c *TarCompressor) Compress(compressedFile io.Writer, targetDir string, fil
 
 	gw := gzip.NewWriter(compressedFile)
 	defer gw.Close()
+
 	tw := tar.NewWriter(gw)
 
-	for _, file := range files {
-		body, _ := ioutil.ReadFile(targetDir + "/" + file)
-		hdr := &tar.Header{
-			Name: file,
-			Mode: 0600,
-			Size: int64(len(body)),
-		}
-		if err := tw.WriteHeader(hdr); err != nil {
+	for _, filename := range files {
+		filepath := fmt.Sprintf("%s/%s", targetDir, filename)
+
+		info, err := os.Stat(filepath)
+		if err != nil {
 			return err
 		}
 
-		if _, err := tw.Write(body); err != nil {
+		if info.IsDir() {
+			continue
+		}
+
+		file, err := os.Open(filepath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		hdr, err := tar.FileInfoHeader(info, "")
+		if err != nil {
+			return err
+		}
+
+		hdr.Name = filename
+		if err := tw.WriteHeader(hdr); err != nil {
+			log.Print(err)
+			return err
+		}
+
+		if _, err = io.Copy(tw, file); err != nil {
 			return err
 		}
 	}
