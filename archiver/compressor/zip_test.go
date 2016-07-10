87714f00f6b522356944fd65203e5a104084c8a0
shinofara <shinofara@gmail.com>
2016-07-10 22:51:49 +0900

    不要なファイル削除

--- archiver/compressor/zip_test.go
+++ archiver/compressor/zip_test.go
@@ -3,11 +3,19 @@ package compressor
 import (
 	"os"
 	"testing"
+
+	"github.com/shinofara/stand/find"	
 )
 
 func TestCreateFileHeader(t *testing.T) {
 	info, _ := os.Stat("testdata/sample.txt")
-	zipHeader, _ := createZipFileHeader("test/sample.txt", info)
+
+	fInfo := find.File{
+		Info: info,
+		Path: "test/sample.txt",
+	}
+	
+	zipHeader, _ := createZipFileHeader(fInfo)
 
 	if info.ModTime() == zipHeader.ModTime() {
 		t.Error("Must not be equall")
