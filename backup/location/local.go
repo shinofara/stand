60b4fb173085742f72fd7afa0a25ff1637867cd3
shinofara <shinofara@gmail.com>
2016-07-12 02:25:53 +0900

    新しいFindの仕様に則って変更

--- backup/location/local.go
+++ backup/location/local.go
@@ -5,7 +5,7 @@ import (
 	"github.com/shinofara/stand/find"
 	"os"
 	"sort"
-
+	"fmt"
 	"bytes"
 )
 
@@ -35,8 +35,73 @@ func (l *Local) Save(filename string, buf *bytes.Buffer) error {
 	return nil
 }
 
+type FindFiles []File
+type File struct {
+	Info     os.FileInfo
+	Path     string
+	FullPath string
+}
+
+func (fi FindFiles) Len() int {
+	return len(fi)
+}
+func (fi FindFiles) Swap(i, j int) {
+	fi[i], fi[j] = fi[j], fi[i]
+}
+func (fi FindFiles) Less(i, j int) bool {
+	return fi[j].Info.ModTime().Unix() < fi[i].Info.ModTime().Unix()
+}
+
+//Clean contains the information about the cleaning.
+type Clean struct {
+	targets FindFiles
+	storageCfg *config.StorageConfig 
+}
+
+//New creates a new Clean
+func NewCLean(storageCfg *config.StorageConfig) *Clean {
+	return &Clean{
+		storageCfg: storageCfg, 
+	}
+}
+
+//Run removes the old generation of the file.
+func (c *Clean) Run() error {
+	f := find.New(c.findMiddleware, c.storageCfg.Path, find.NotDeepSearchMode, find.FileOnlyMode)
+	if err := f.Run(); err != nil {
+		return err
+	}
+
+	for _, file := range c.targets {
+		fmt.Println(file)
+	}
+	return nil
+}
+
+//findMiddleware is middeware of find.findCallBack interface's
+func (c *Clean) findMiddleware(path string, file *os.File) error {
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	fullPath := fmt.Sprintf("%s/%s", c.storageCfg.Path, path)
+	
+	fInfo := File{
+		Info:     info,
+		Path:     path,
+		FullPath: fullPath}
+	
+	c.targets = append(c.targets, fInfo)
+	return nil
+}
+
 func (l *Local) Clean() error {
-	files, _ := find.Find(l.storageCfg.Path, false, true)
+	c := NewCLean(l.storageCfg)
+	if err := c.Run(); err != nil {
+		panic(err)
+	}
+	files := c.targets
 	sort.Sort(files)
 
 	var num int64 = 0
