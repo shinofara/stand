eddccafc1ab6beb28a56407252b7c9450ff96ce1
shinofara <shinofara@gmail.com>
2016-08-25 02:13:45 +0900

    クロスコンパイル幅を増やす

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-for os in darwin linux
+for os in darwin linux freebsd openbsd
 do
-    for arch in amd64
+    for arch in amd64 386
     do
 	      GOOS=${os} GOARCH=${arch} go build -o ../../stand_${os}_${arch} -ldflags="-w -s";
     done
