27f58c3d79639778b280f478764abdc3747257f7
shinofara <shinofara@gmail.com>
2016-09-24 14:38:46 +0900

    CIのデバック終了

--- config/config_test.go
+++ config/config_test.go
@@ -8,7 +8,7 @@ import (
 func TestLoad(t *testing.T) {
 	expected := &Configs{
 		&Config{
-			Type: "dira",
+			Type: "dir",
 			Path: "/path/to/target/1",
 			StorageConfigs: []StorageConfig{
 				{
